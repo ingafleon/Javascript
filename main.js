54eb3961210d4f159b96a136159bb60fe9b0a714
ingafleon <ing.afleon@gmail.com>
2018-07-12 19:48:32 -0500

    funcion login

---
+++ main.js
@@ -0,0 +1,15 @@
+var variable_1 = 'Hola Mundo';
+var alcance = 10;
+var pDato = document.getElementById('dato');
+var dat = [];
+
+// for(var i = 0; i < alcance; i++ ){
+//     console.log(i);
+//     dat.push(i);
+//     pDato.innerHTML = dat;
+// }
+function login(){
+    var usuario1 = document.getElementById('nombre_1').value;
+    var contrasena1 = document.getElementById('contrasena_1').value;
+document.getElementById('login')
+}
